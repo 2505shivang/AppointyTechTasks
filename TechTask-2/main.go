7d54b9235407abb54a42fc585af5ba44fd3eae43
2505shivang <2505shivang@gmail.com>
2020-10-15 02:38:00 +0530

    Pagination added

--- TechTask-2/main.go
+++ TechTask-2/main.go
@@ -6,7 +6,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -22,6 +24,23 @@ type Article struct {
 
 var Articles []Article
 
+var mutex = &sync.Mutex{}
+
+//Pagination
+
+func Pagination(r *http.Request, limit int) (int, int) {
+	keys := r.URL.Query()
+	if keys.Get("page") == "" {
+		return 1, 0
+	}
+	page, _ := strconv.Atoi(keys.Get("page"))
+	if page < 1 {
+		return 1, 0
+	}
+	begin := (limit * page) - limit
+	return page, begin
+}
+
 //handlers
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -30,9 +49,40 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	limit := 5
+	page, begin := Pagination(r, limit)
 
+	var results []Article
+	total := len(Articles)
+	pages := (total / limit)
+	if (total % limit) != 0 {
+		pages++
+	}
+	mutex.Lock()
+	if page*limit > total {
+		results = Articles[begin:total]
+	} else {
+		results = Articles[begin : page*limit]
+	}
+	mutex.Unlock()
+
+	fmt.Printf("Current Page: %d, Begin: %d\n", page, begin)
 	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
+	json.NewEncoder(w).Encode(struct {
+		Total    int       `json:"total"`
+		Page     int       `json:"page"`
+		Pages    int       `json:"pages"`
+		NextPage int       `json:"nextpage"`
+		PrevPage int       `json:"previouspage"`
+		Articles []Article `json:"docs"`
+	}{
+		Total:    total,
+		Page:     page,
+		Pages:    pages,
+		NextPage: page + 1,
+		PrevPage: page - 1,
+		Articles: results,
+	})
 }
 
 func createNewArticle(w http.ResponseWriter, r *http.Request) {
@@ -41,10 +91,12 @@ func createNewArticle(w http.ResponseWriter, r *http.Request) {
 	var article Article
 	json.Unmarshal(reqBody, &article)
 
+	mutex.Lock()
 	article.CreationTimestamp = time.Now()
-
 	Articles = append(Articles, article)
+	defer mutex.Unlock()
 	json.NewEncoder(w).Encode(article)
+
 }
 
 func articles(w http.ResponseWriter, r *http.Request) {
@@ -70,11 +122,13 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	}
 	key := parts[2]
 
+	mutex.Lock()
 	for _, article := range Articles {
 		if article.ID == key {
 			json.NewEncoder(w).Encode(article)
 		}
 	}
+	defer mutex.Unlock()
 
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Println(key)
@@ -89,20 +143,18 @@ func searchquery(w http.ResponseWriter, r *http.Request) {
 
 	var FoundArticles []Article
 
+	mutex.Lock()
 	for _, article := range Articles {
 
 		intitle := strings.Contains(strings.ToLower(article.Title), strings.ToLower(v))
 		inSubtitle := strings.Contains(strings.ToLower(article.Subtitle), strings.ToLower(v))
 		inContent := strings.Contains(strings.ToLower(article.Content), strings.ToLower(v))
 
-		fmt.Println(inSubtitle)
-		fmt.Println(intitle)
-		fmt.Println(inContent)
-
 		if intitle == true || inSubtitle == true || inContent == true {
 			FoundArticles = append(FoundArticles, article)
 		}
 	}
+	defer mutex.Unlock()
 
 	json.NewEncoder(w).Encode(FoundArticles)
 	return
